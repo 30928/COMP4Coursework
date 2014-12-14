a204debc772c77297cdd30ad35a127ffacfc236d
Imran Rahman <mail.fake>
2014-12-14 21:19:38 +0000

    Implementation started

---
+++ Implementation/Database/Database.py
@@ -0,0 +1,367 @@
+import sqlite3
+
+#inserting data
+def create_data(data, table, placeholders):
+    with sqlite3.connect("PP.db") as db:
+        cursor = db.cursor()
+        cursor.execute("PRAGMA foreign_keys_ = ON")
+        sql = "insert into {} values {}".format(table, placeholders)
+        cursor.execute(sql, data)
+        db.commit()
+
+#updating data
+def update_data(data, table, update, ID):
+    with sqlite3.connect("PP.db") as db:
+        cursor = db.cursor()
+        cursor.execute("PRAGMA foreign_keys_ = ON")
+        sql = "update {} set {} where {} = {}".format(table, update, ID, data)
+        print(sql)
+        cursor.execute(sql)
+        db.commit()
+
+#deleting data
+def delete_data(data, table, ID):
+    try:
+        with sqlite3.connect("PP.db") as db:
+            cursor = db.cursor()
+            cursor.execute("PRAGMA foreign_keys_ = ON")
+            sql = "delete from {} where {} = {}".format(table, ID, data)
+            cursor.execute(sql)
+            db.commit()
+    except:
+        print("Error")
+        
+#place holders and table contents for adding entries
+def CustomerEntry():
+    table = "Customer (FirstName, LastName, Email, PhoneNumber, Address, Postcode)"
+    placeholders = "(?, ?, ?, ?, ?, ?)"
+    return table, placeholders
+
+def BookEntry():
+    table = "Book (ISBN, AuthorID, BookTitle, NoOfPages, Size, Back, Cover, Paper, Font, FontSize, DatePublished, Price)"
+    placeholders = "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    return table, placeholders
+    
+def PubInvoiceEntry():
+    table = "PubInvoice (AuthorID, ISBN, PubInvoiceDate, PubInvoiceService, PubInvoicePayment)"
+    placeholders = "(?, ?, ?, ?, ?)"
+    return table, placeholders
+
+def BookInvoiceEntry():
+    table = "BookInvoice (AuthorID, BookInvoicePayment, BookInvoiceDate)"
+    placeholders = "(?, ?, ?)"
+    return table, placeholders
+
+def BookInvoiceItemsEntry():
+    table = "BookInvoiceItems (BookInvoiceID, ISBN, BookInvoiceQuantity, BookInvoiceDiscount, ShippingType, ShippingPrice)"
+    placeholders = "(?, ?, ?, ?, ?, ?)"
+    return table, placeholders
+
+def RoyaltiesEntry():
+    table = "Royalties (AuthorID, RoyaltyPayment, RoyaltiesDate)"
+    placeholders = "(?, ?, ?)"
+    return table, placeholders
+
+def RoyaltiesItemsEntry():
+    table = "RoyaltiesItems (RoyaltiesID, ISBN, Currency, RoyaltyDiscount, WholesalePrice, RoyaltyQuantity, PrintCost, NetSales, ExcRateFromGBP)"
+    placeholders = "(?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    return table, placeholders
+
+#retrieving input data from user
+def input_customer():
+    firstname = input("Enter first name: ")
+    lastname = input("Enter last name: ")
+    email = input("Enter Email: ")
+    phonenumber = input("Enter phone number: ")
+    address = input("Enter address: ")
+    postcode = input("Enter postcode: ")
+    input_data = (firstname, lastname, email, phonenumber, address, postcode)
+    return input_data
+
+def input_book():
+    ISBN = input("Enter ISBN: ")
+    AuthorID = input("Enter AuthorID: ")
+    BookTitle = input("Enter book title: ")
+    NoOfPages = input("Enter number of pages: ")
+    Size = input("Enter size: ")
+    Back = input("Enter back type: ")
+    Cover = input("Enter cover type: ")
+    Paper = input("Enter paper type: ")
+    Font = input("Enter font: ")
+    FontSize = input("Enter font size: ")
+    DatePublished = input("Enter date published: ")
+    Price = input("Enter price: ")
+    input_data = (ISBN, AuthorID, BookTitle, NoOfPages, Size, Back, Cover, Paper, Font, FontSize, DatePublished, Price)
+    return input_data
+
+def input_pub_invoice():
+    AuthorID = input("Enter AuthorID: ")
+    ISBN = input("Enter ISBN: ")
+    PubInvoiceDate = input("Enter publishing invoice date: ")
+    PubInvoiceService = input("Enter publishing invoice service: ")
+    PubInvoicePayment = input("Enter publishing invoice payment: ")
+    input_data = (AuthorID, ISBN, PubInvoiceDate, PubInvoiceService, PubInvoicePayment)
+    return input_data
+
+def input_book_invoice():
+    AuthorID = input("Enter AuthorID: ")
+    BookInvoiceDate = input("Enter book invoice date: ")
+    BookInvoicePayment = None
+
+    input_data = (AuthorID, BookInvoicePayment, BookInvoiceDate)
+    return input_data
+
+def input_book_invoice_items():
+    BookInvoiceID = input("Enter BookInvoiceID: ")
+    ISBN = input("Enter ISBN: ")
+    BookInvoiceQuantity = float(input("Enter quantity bought: "))
+    BookInvoiceDiscount = float(input("Enter invoice discount: "))
+    ShippingType = input("Enter shipping type: ")
+    ShippingPrice = input("Enter shipping price: ")
+    input_data = (BookInvoiceID, ISBN, BookInvoiceQuantity, BookInvoiceDiscount, ShippingType, ShippingPrice)
+    return input_data
+
+def input_royalties():
+    AuthorID = input("Enter AuthorID: ")
+    RoyaltyPayment = None
+    RoyaltiesDate = input("Enter royalties date: ")
+    input_data = (AuthorID, RoyaltyPayment, RoyaltiesDate)
+    return input_data
+
+def input_royalties_items():
+    RoyaltiesID = input("Enter RoyaltiesID: ")
+    ISBN = input("Enter ISBN: ")
+    Currency = input("Enter currency: ")
+    RoyaltyDiscount = input("Enter royalty discount: ")
+    WholesalePrice = float(input("Enter wholesale price: "))
+    RoyaltyQuantity = int(input("Enter quantity bought: "))
+    #PrintCost = float(input("Enter print cost: "))
+    PrintCost = None
+    ExcRateFromGBP = None
+    NetSales = WholesalePrice * RoyaltyQuantity
+    #RoyaltiesItemPayment = NetSales - PrintCost
+    if Currency != "£":
+        ExcRateFromGBP = float(input("Enter current exchange rate from pounds: "))
+        #RoyaltiesItemPayment = RoyaltiesItemPayment / ExcRateFromGBP
+        
+    input_data = (RoyaltiesID, ISBN, Currency, RoyaltyDiscount, WholesalePrice, RoyaltyQuantity, PrintCost, NetSales, ExcRateFromGBP)
+    return input_data
+
+#Displaying the Main Menu
+def DisplayMainMenu():
+    print()
+    print('MAIN MENU')
+    print()
+    print('1. Add Data')
+    print('2. Update Data')
+    print('3. Remove Data')
+    print()
+    print('Select an option from the menu (or enter q to quit): ', end='')
+
+
+#Displaying the Menu for Adding entries
+def DisplayAddMenu():
+    print()
+    print('ADD MENU')
+    print()
+    print('1. Add Customer Entry')
+    print('2. Add Book')
+    print('3. Add Publishing Invoice')
+    print('4. Add Book Invoice')
+    print('5. Add Book Invoice Items')
+    print('6. Add Royalties')
+    print('7. Add Royalties Items')
+    print()
+    print('Select an option from the menu (or enter b to go back): ', end='')
+
+
+#Displaying the Menu for Updating Entries
+def DisplayUpdateMenu():
+    print()
+    print("UPDATE MENU")
+    print()
+    print("1. Update Customer")
+    print("2. Update Book")
+    print("3. Update Publishing Invoice")
+    print("4. Update Book Invoice")
+    print("5. Update Book Invoice Items")
+    print("6. Update Royalties")
+    print("7. Update Royalties Items")
+    print()
+    print('Select an option from the menu (or enter b to go back): ', end='')
+
+#Displaying the Menu for Deleting Entries
+def DisplayDeleteMenu():
+    print()
+    print("DELETE MENU")
+    print()
+    print("1. Delete Customer")
+    print("2. Delete Book")
+    print("3. Delete Publishing Invoice")
+    print("4. Delete Book Invoice")
+    print("5. Delete Book Invoice Item")
+    print("6. Delete Royalty")
+    print("7. Delete Royalties Item")
+    print()
+    print('Select an option from the menu (or enter b to go back): ', end='')
+
+#Getting a choice for the main menu/add menu from the user
+def GetMenuChoice():
+  Choice = input()
+  print()
+  return Choice
+
+#Getting a choice for the update menu from the user
+def GetUpdateChoice():
+
+    tablepick = input()
+
+    if tablepick == "1":
+        ID = "AuthorID"
+        table = "Customer"
+    elif tablepick == "2":
+        ID = "ISBN"
+        table = "Book"
+    elif tablepick == "3":
+        ID = "PubInvoiceID"
+        table = "PubInvoice"
+    elif tablepick == "4":
+        ID = "BookInvoiceID"
+        table = "BookInvoice"
+    elif tablepick == "5":
+        ID = "BookInvoiceItems"
+        table = "BookInvoiceItems"
+    elif tablepick == "6":
+        ID = "RoyaltiesID"
+        table = "Royalties"
+    elif tablepick == "7":
+        ID = "RoyaltiesItems"
+        table = "RoyaltiesItems"
+    data = input("Enter the {} for the entry you want to update: ".format(ID))
+
+    return table, data, ID
+
+def GetUpdateData():
+    updatelist = []
+    count = 0
+    entry = None
+    value = []
+    while entry != "n":
+        if count == 0:
+            entry = input("Enter what you want to update: ")
+            updatelist.append(entry)
+            count = 1
+        elif count == 1:
+            entry = input("Enter what you want to update (or enter n to update): ")
+            
+            if entry != "n":
+                updatelist.append(entry)
+            
+    for count in range(0, len(updatelist)):
+        rawvalue = input("Enter the new value for the {}: ".format(updatelist[count]))
+        rawvalue = "'" + rawvalue + "'"
+        value.append(rawvalue)
+        
+    update = ""
+    for count in range(0, len(updatelist)):
+        if count == 0:
+            update += "{} = {}".format(updatelist[count], value[count])
+        else:
+            update += ", {} = {}".format(updatelist[count], value[count])
+
+    return update
+                                   
+#Getting a choice and data for the delete menu from the user
+def GetDeleteChoice():
+
+    tablepick = input()
+
+    if tablepick == "1":
+        ID = "AuthorID"
+        table = "Customer"
+    elif tablepick == "2":
+        ID = "ISBN"
+        table = "Book"
+    elif tablepick == "3":
+        ID = "PubInvoiceID"
+        table = "PubInvoice"
+    elif tablepick == "4":
+        ID = "BookInvoiceID"
+        table = "BookInvoice"
+    elif tablepick == "5":
+        ID = "BookInvoiceItems"
+        table = "BookInvoiceItems"
+    elif tablepick == "6":
+        ID = "RoyaltiesID"
+        table = "Royalties"
+    elif tablepick == "7":
+        ID = "RoyaltiesItems"
+        table = "RoyaltiesItems"
+
+    data = input("Enter the {}: ".format(ID))
+
+    return data, table, ID
+
+#main function
+def main():
+
+    Choice = '0'
+
+    while Choice not in ['q', 'Q','Quit', 'quit']:
+
+        DisplayMainMenu()
+        Choice = GetMenuChoice()
+
+        if Choice == '1':
+
+            while Choice not in ['b', 'B','Back', 'back']:
+                DisplayAddMenu()
+                Choice = GetMenuChoice()
+                
+                if Choice == '1':
+                    customer_data = input_customer()
+                    table, placeholders = CustomerEntry()
+                    create_data(customer_data, table, placeholders)
+                elif Choice == '2':
+                    book_data = input_book()
+                    table, placeholders = BookEntry()
+                    create_data(book_data, table, placeholders)
+                elif Choice == '3':
+                    pub_invoice_data = input_pub_invoice()
+                    table, placeholders = PubInvoiceEntry()
+                    create_data(pub_invoice_data, table, placeholders)
+                elif Choice == '4':
+                    book_invoice_data = input_book_invoice()
+                    table, placeholders = BookInvoiceEntry()
+                    create_data(book_invoice_data, table, placeholders)
+                elif Choice == '5':
+                    book_invoice_items_data = input_book_invoice_items()
+                    table, placeholders = BookInvoiceItemsEntry()
+                    create_data(book_invoice_items_data, table, placeholders)
+                elif Choice == '6':
+                    royalties_data = input_royalties()
+                    table, placeholders = RoyaltiesEntry()
+                    create_data(royalties_data, table, placeholders)
+                elif Choice == '7':
+                    royalties_items_data = input_royalties_items()
+                    table, placeholders = RoyaltiesItemsEntry()
+                    create_data(royalties_items_data, table, placeholders)
+
+        if Choice == '2':
+
+            while Choice not in ['b', 'B', 'Back', 'back']:
+                DisplayUpdateMenu()
+                table, data, ID = GetUpdateChoice()
+                update = GetUpdateData()
+                update_data(data, table, update, ID)
+        
+        if Choice == '3':
+
+            while Choice not in ['b', 'B', 'Back', 'back']:
+                
+                DisplayDeleteMenu()
+                data, table, ID = GetDeleteChoice()
+                delete_data(data, table, ID)
+            
+if __name__ == "__main__":
+    main()
---
+++ Implementation/Database/InitDB.py
@@ -0,0 +1,133 @@
+import sqlite3
+
+def create_table(db_name, table_name, sql):
+    with sqlite3.connect(db_name) as db:
+        cursor = db.cursor()
+        cursor.execute("PRAGMA foreign_keys_ = ON")
+        cursor.execute("select name from sqlite_master where name=?", (table_name,))
+        result = cursor.fetchall()
+        keep_table = True
+        if len(result) == 1:
+            response = input("The table {0} already exists, do you wish to recreate it (y/n): ".format(table_name))
+            if response == "y":
+                keep_table = False
+                print("The {0} table will be recreated - all existing data will be lost".format(table_name))
+                cursor.execute("drop table if exists {0}".format(table_name))
+                db.commit()
+            else:
+                print("The existing table was kept")
+        else:
+            keep_table = False
+        if not keep_table:
+            cursor.execute(sql)
+            db.commit()
+
+def customer_table(db_name):
+    sql = """create table Customer 
+             (AuthorID integer,
+             FirstName text,
+             LastName text,
+             Email text,
+             PhoneNumber text,
+             Address text,
+             Postcode text,
+             primary key(AuthorID))"""
+    create_table(db_name, "Customer", sql)
+        
+def book_table(db_name):
+    sql = """create table Book 
+             (ISBN text,
+             AuthorID integer,
+             BookTitle text,
+             NoOfPages integer,
+             Size text,
+             Back text,
+             Cover text,
+             Paper text,
+             Font text,
+             FontSize real,
+             DatePublished date,
+             Price real,
+             primary key(ISBN),
+             foreign key(AuthorID) references Customer(AuthorID))"""
+    create_table(db_name, "Book", sql)
+
+def pub_invoice_table(db_name):
+    sql = """create table PubInvoice 
+             (PubInvoiceID integer,
+             ISBN text,
+             AuthorID integer,
+             PubInvoiceDate date,
+             PubInvoiceService text,
+             PubInvoicePayment real,
+             primary key(PubInvoiceID),
+             foreign key(AuthorID) references Customer(AuthorID),
+             foreign key(ISBN) references Book(ISBN))"""
+    create_table(db_name, "PubInvoice", sql)
+
+def book_invoice_table(db_name):
+    sql = """create table BookInvoice
+             (BookInvoiceID integer,
+             AuthorID integer,
+             BookInvoiceDate date,
+             BookInvoicePayment real,
+             primary key(BookInvoiceID),
+             foreign key(AuthorID) references Customer(AuthorID))"""
+    create_table(db_name, "BookInvoice", sql)
+
+def book_invoice_items_table(db_name):
+    sql = """create table BookInvoiceItems
+             (BookInvoiceItems,
+             BookInvoiceID integer,
+             ISBN text,
+             BookInvoiceQuantity integer,
+             BookInvoiceDiscount real,
+             ShippingType text,
+             ShippingPrice real,
+             primary key(BookInvoiceItems),
+             foreign key(BookInvoiceID) references BookInvoice(BookInvoiceID),
+             foreign key(ISBN) references Book(ISBN))"""
+    create_table(db_name, "BookInvoiceItems", sql)
+
+def royalties_table(db_name):
+    sql = """create table Royalties
+             (RoyaltiesID integer,
+             AuthorID integer,
+             RoyaltiesDate date,
+             RoyaltyPayment real,
+             primary key(RoyaltiesID),
+             foreign key(AuthorID) references Customer(AuthorID))"""
+    create_table(db_name, "Royalties", sql)
+
+def royalties_items_table(db_name):
+    sql = """create table RoyaltiesItems
+             (RoyaltiesItems integer,
+             RoyaltiesID integer,
+             ISBN text,
+             Currency text,
+             RoyaltyDiscount real,
+             WholesalePrice real,
+             RoyaltyQuantity integer,
+             NetSales real,
+             PrintCost real,
+             ExcRateFromGBP real,
+             primary key(RoyaltiesItems),
+             foreign key(RoyaltiesID) references Royalties(RoyaltiesID),
+             foreign key(ISBN) references Book(ISBN))"""
+    create_table(db_name, "RoyaltiesItems", sql)
+
+def main():
+    db_name = "PP.db"
+    customer_table(db_name)
+    book_table(db_name)
+    pub_invoice_table(db_name)
+    book_invoice_table(db_name)
+    book_invoice_items_table(db_name)
+    royalties_table(db_name)
+    royalties_items_table(db_name)
+
+    
+
+if __name__ == "__main__":
+    main()
+    
