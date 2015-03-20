bde1dedff81f9bfc9510c1a886af02ce13607f33
30928 <30928@PC004153.coll.lr.local>
2015-03-20 12:14:54 +0000

    development on variable listing

--- Implementation/Database/MainMenu.py
+++ Implementation/Database/MainMenu.py
@@ -316,6 +316,7 @@ class MainWindow(QMainWindow):
             self.ConfirmDialog.Msg = "Are you sure you want to delete this Item?"
             self.ConfirmDialog.ConfirmedMsg = "Item was successfully deleted"
         self.ConfirmDialog.Prevention = True #deletion may be prevented if integrity error occurs
+        
         if self.SelectedRow != -1:
             self.ConfirmDialog.VerifyDlg() #verification
             try:
